6a8d3a47b224d5e5893731f8169394ddfa63fba0
Bruno0190 <bruno.varadero90@gmail.com>
2026-01-25 17:08:29 +0100

    first commit

--- src/main/java/curriculum_vitae/cv/controller/CvController.java
+++ src/main/java/curriculum_vitae/cv/controller/CvController.java
@@ -1,5 +1,12 @@
 package curriculum_vitae.cv.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class CvController {
 
+    @GetMapping("/")
+    public String cv() {
+        return "index"; // index.html in templates/
+    }
 }
--- src/main/java/curriculum_vitae/cv/controller/PdfController.java
+++ src/main/java/curriculum_vitae/cv/controller/PdfController.java
@@ -1,5 +1,48 @@
 package curriculum_vitae.cv.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.nio.file.Files;
+import java.io.IOException;
 
+@RestController
 public class PdfController {
 
+    @GetMapping("/genera-pdf")
+    public void generaPdf(HttpServletResponse response) throws IOException {
+
+        // URL della tua pagina HTML
+        String inputUrl = "http://localhost:8080/cv"; // <-- la tua pagina CV
+
+        // File temporaneo
+        File tempPdf = File.createTempFile("cv", ".pdf");
+
+        // Comando wkhtmltopdf
+        ProcessBuilder pb = new ProcessBuilder(
+                "wkhtmltopdf",
+                "--page-size", "A4",
+                "--margin-top", "5mm",
+                "--margin-bottom", "5mm",
+                "--margin-left", "5mm",
+                "--margin-right", "5mm",
+                inputUrl,
+                tempPdf.getAbsolutePath()
+        );
+
+        Process process = pb.start();
+
+        try {
+            process.waitFor();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Imposta headers per il download
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition", "attachment; filename=cv.pdf");
+
+        // Copia il PDF nella risposta
+        Files.copy(tempPdf.toPath(), response.getOutputStream());
+    }
 }
