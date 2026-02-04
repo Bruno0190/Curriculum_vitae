81ac41f16d3c30dc81e55d2c15d822c65ecd2dac
Bruno0190 <bruno.varadero90@gmail.com>
2026-02-04 16:59:23 +0100

    feat: model and relations II

--- src/main/java/curriculum_vitae/cv/model/skill.java
+++ src/main/java/curriculum_vitae/cv/model/skill.java
@@ -9,4 +9,26 @@ import jakarta.persistence.Table;
 @Table(name = "skills")
 public class skill {
 
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String skill_name;
+
+    public skill() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getSkill_name() {
+        return skill_name;
+    }
+
+    public void setSkill_name(String skill_name) {
+        this.skill_name = skill_name;
+    }
+    
 }
