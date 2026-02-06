54c61f8f9e06001117ed6d06f68e53279cfee030
Bruno0190 <bruno.varadero90@gmail.com>
2026-02-06 20:14:10 +0100

    repository II

---
+++ src/main/java/curriculum_vitae/cv/repository/SkillRepository.java
@@ -0,0 +1,10 @@
+package curriculum_vitae.cv.repository;
+
+import curriculum_vitae.cv.model.Skill;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SkillRepository extends JpaRepository<Skill, Long> {
+
+}
